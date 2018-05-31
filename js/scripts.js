261cc42d48877e618c725793e4ff2203043c0da5
Virlie Paglinawan <Guest@Epicodus-7B.local>
2018-05-30 19:22:44 -0700

    add information on README.md file

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User interface (or front-end) logic:
 
 $(document).ready(function() {
-  var number1 = parseInt(prompt("enter a number:"));
-  var number2 = parseInt(prompt("enter another number:"));
-
-  alert(add(number1,number2)); s
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
 });
