664ebd9a3df0dd54f3d79b3e91e9e27b636d74c3
Virlie Paglinawan <Guest@Epicodus-7B.local>
2018-05-30 19:10:46 -0700

    make the addition functional

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+$(document).ready(function() {
+  var number1 = parseInt(prompt("enter a number:"));
+  var number2 = parseInt(prompt("enter another number:"));
+
+  alert(add(number1,number2)); s
+});
